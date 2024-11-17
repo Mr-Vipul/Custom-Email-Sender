1a3b7cc071f7265862be17507588a40047bc1344
Vipul <goyalvipul2002@gmail.com>
2024-11-17 23:09:26 +0530

    Groq Api

--- groq_integration.py
+++ groq_integration.py
@@ -2,6 +2,6 @@ import groq
 
 def generate_personalized_email(prompt):
     """Generate personalized email using Groq API"""
-    client = groq.Client(api_key="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")
+    client = groq.Client(api_key="your_groq_Api")
     response = client.query(prompt)
     return response.get("text", "")
